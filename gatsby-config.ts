d92dc23dccd511ec924b0fb081fb2c4fe64191ea
Michele Moio <michele.moio@protonmail.com>
2023-04-18 10:20:30 +0200

    feat(CPP-83): SEO & Helmet metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,13 +1,24 @@
 import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-	siteMetadata: {
-		title: `Firma con IO`,
-		siteUrl: `https://www.yourdomain.tld`,
-	},
+  siteMetadata: {
+		metaTitle: `Firma con IO`,
+		siteUrl: `https://www.pagopa.firmaconio.it`, // TODO change to actual url
+    metaDescription: ``,
+    metaSocial: {
+      socialNetwork: `twitter`,
+      title: `https://www.twitterdomain.tld`,
+      description: `author`,
+      image: `/site-icon.png`,
+    },
+    keywords: ``,
+    metaRobots: ``,
+    metaViewport: `width=device-width, initial-scale=1, shrink-to-fit=no`,
+  },
 	graphqlTypegen: true,
 	plugins: [
 		`gatsby-transformer-yaml`,
+    `gatsby-plugin-react-helmet`,
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
--- src/pages/{pageYaml.route}/{pageYaml.url}.tsx
+++ src/pages/{pageYaml.route}/{pageYaml.url}.tsx
@@ -1,23 +1,41 @@
 import { graphql } from 'gatsby';
 import React from 'react';
 import { BlocksRenderer } from '../../components/BocksRenderer';
+import { SEO } from '../../components/SEO';
 
 const MdxPage = ({
 	data: {
-		pageYaml: {
-			components
-		},
+		pageYaml: { components, seo },
 	},
 }: {
 	data: Queries.PageYamlQuery;
 }) => {
-	return <BlocksRenderer blocks={components} />;
-}
+	return (
+		<>
+			<SEO meta={seo} />
+			<BlocksRenderer blocks={components} />
+		</>
+	);
+};
 
 export const query = graphql`
 	query PageYaml($id: String) {
 		pageYaml(id: { eq: $id }) {
 			...Components
+			seo {
+				metaDescription
+				metaImage {
+					localFile {
+						publicURL
+					}
+				}
+				metaSocial {
+					description
+					socialNetwork
+					title
+				}
+				metaTitle
+			}
 		}
 	}
 `;
