b37292c258b5b5052842f9def6592fd5dbbb6dd9
Michele Moio <michele.moio@protonmail.com>
2023-04-21 17:04:20 +0200

    fix(CPP-87): hero background WIP

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,8 +8,26 @@ export const Hero = ({ background, altText, ...props }: HeroProps) => {
 		<Pagopa.Hero
 			{...props}
 			background={
-				<div style={{ display: 'flex', justifyContent: 'center' }}>
-					<GatsbyImage image={getImage(background)} alt={altText} style={{ position: 'absolute', zIndex: -1, width: '100%' }} />
+				<div
+					className="lollo"
+					style={{
+						backgroundImage:
+							'linear-gradient(0deg, rgba(0, 98, 195, 0.65), rgba(0, 98, 195, 0.65))',
+						backgroundColor: 'rgb(0, 115, 230)',
+						position: 'absolute',
+						width: '100%',
+						height: '100%',
+						opacity: 0.5,
+						left: 0,
+						top: 0,
+						zIndex: -1,
+					}}
+				>
+					<GatsbyImage
+						image={getImage(background)}
+						alt={altText}
+						style={{ zIndex: -2 }}
+					/>
 				</div>
 			}
 		/>
