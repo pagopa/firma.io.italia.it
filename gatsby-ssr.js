4cc4a68c43ac9c09005f10e9f4fac6ab302778d9
Vito Falco <vitofalco@gmail.com>
2023-06-08 17:47:47 +0200

    Add matomojs

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { ThemeProvider } from '@mui/material';
-import { theme } from './src/theme';
-import CssBaseline from '@mui/material/CssBaseline';
+import React from "react";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./src/theme";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const matomo = () => {
-	const html = `
+  const html = `
     var _paq = window._paq = window._paq || [];
     /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
     _paq.push(['trackPageView']);
@@ -14,31 +14,31 @@ const matomo = () => {
       _paq.push(['setTrackerUrl', u+'matomo.php']);
       _paq.push(['setSiteId', '6']);
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-      g.async=true; g.src='//cdn.matomo.cloud/pagopa.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+      g.async=true; g.src='/js/matomo.js'; s.parentNode.insertBefore(g,s);
     })();
   `;
 
-	return (
-		<script
-			key="script-gatsby-plugin-matomo"
-			dangerouslySetInnerHTML={{ __html: html }}
-		/>
-	);
+  return (
+    <script
+      key="script-gatsby-plugin-matomo"
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+  );
 };
 
 export const wrapRootElement = ({ element }) => {
-	return (
-		<>
-			<CssBaseline />
-			<ThemeProvider theme={theme}>{element}</ThemeProvider>
-		</>
-	);
+  return (
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>{element}</ThemeProvider>
+    </>
+  );
 };
 
 export const onRenderBody = ({ setPostBodyComponents }) => {
-	const isProduction = process.env.NODE_ENV === 'production';
+  const isProduction = process.env.NODE_ENV === "production";
 
-	if (isProduction) {
-		setPostBodyComponents([matomo()]);
-	}
+  if (isProduction) {
+    setPostBodyComponents([matomo()]);
+  }
 };
