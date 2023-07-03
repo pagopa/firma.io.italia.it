41ae8d5ca38a453298fba6d65d3dd13ba1ad4156
silvicir <127971996+silvicir@users.noreply.github.com>
2023-07-03 18:20:04 +0200

    add PagoPA and Namirial paragraphs in termini e condizioni Firma con IO

--- src/components/Terms.tsx
+++ src/components/Terms.tsx
@@ -10,12 +10,16 @@ const Terms = (_props: TermsProps) => {
     <Box sx={{ py: { xs: 4, md: 10 } }} component="section">
       <Container>
         <h4>I termini e le condizioni</h4>
+        <p><strong>PagoPA</strong></p>
         <ul>
-          <li><a href="https://io.italia.it/app-content/tos_privacy.html" target="_blank" rel="noopener">https://io.italia.it/app-content/tos_privacy.html</a></li>
-          <li><a href="https://docs.namirialtsp.com/documents/Mod_NAM_CA01D_ITA_50.pdf" target="_blank" rel="noopener">https://docs.namirialtsp.com/documents/Mod_NAM_CA01D_ITA_50.pdf</a></li>
-          <li><a href="https://docs.namirialtsp.com/documents/Mod_NAM_GDPR03D_ITA_50.pdf" target="_blank" rel="noopener">https://docs.namirialtsp.com/documents/Mod_NAM_GDPR03D_ITA_50.pdf</a></li>
+          <li><a href="https://io.italia.it/app-content/tos_privacy.html" target="_blank" rel="noopener">TOS_PRIVACY</a></li>
         </ul>
-        <p><em>Data di ultima modifica: 14 giugno 2023</em></p>
+        <p><strong>Namirial</strong></p>
+        <ul>
+          <li><a href="https://docs.namirialtsp.com/documents/Mod_NAM_CA01D_ITA_50.pdf" target="_blank" rel="noopener">Mod_NAM_CA01D_ITA_50.pdf</a></li>
+          <li><a href="https://docs.namirialtsp.com/documents/Mod_NAM_GDPR03D_ITA_50.pdf" target="_blank" rel="noopener">Mod_NAM_GDPR03D_ITA_50.pdf</a></li>
+        </ul>
+        <p><em>Data di ultima modifica: 3 luglio 2023</em></p>
       </Container>
     </Box >)
 };
